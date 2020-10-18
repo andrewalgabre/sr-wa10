784e76ead41ccd5d3e9186db1a90f57f9d352957
Andrew Algabre <a.algabre@me.com>
2020-10-19 00:18:23 +0200

    add some tests

--- src/app/building/containers/building/building.component.spec.ts
+++ src/app/building/containers/building/building.component.spec.ts
@@ -21,4 +21,19 @@ describe('BuildingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have one nickname more', () => {
+    component.onAdd('Archy');
+    expect(component.data.nicknames.length).toBe(3);
+  });
+
+  it('should have one nickname less', () => {
+    component.onDelete(0);
+    expect(component.data.nicknames.length).toBe(1);
+  });
+
+  it('should change the nickname', () => {
+    component.onChange({ index: 0, nickname: 'Andrew' });
+    expect(component.data.nicknames[0]).toBe('Andrew');
+  });
 });
