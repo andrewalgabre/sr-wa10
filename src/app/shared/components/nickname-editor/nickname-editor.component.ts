b23f2a8c6fca830da3f385eadcbb58a213a44e7e
Andrew Algabre <a.algabre@me.com>
2020-10-18 17:48:23 +0200

    add initial form array of nicknames

--- src/app/shared/components/nickname-editor/nickname-editor.component.ts
+++ src/app/shared/components/nickname-editor/nickname-editor.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nickname-editor',
   templateUrl: './nickname-editor.component.html',
-  styleUrls: ['./nickname-editor.component.scss']
+  styleUrls: ['./nickname-editor.component.scss'],
 })
 export class NicknameEditorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() set nickNames(nickNames: string) {
+    // this.formArray.push;
   }
 
+  public nickNameFormList: FormArray;
+  public nickNameAddFormControl: FormControl;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.nickNameFormList = this.formBuilder.array([]);
+
+    this.nickNameAddFormControl = this.formBuilder.control('');
+  }
+
+  ngOnInit(): void {}
+
+  onNicknameAdd() {
+    if (!this.nickNameAddFormControl.valid) return;
+    this.nickNameFormList.controls.push(
+      new FormControl(this.nickNameAddFormControl.value)
+    );
+    this.nickNameAddFormControl.reset();
+  }
+
+  onNicknameDelete() {}
 }
--- src/app/vendor/angular-material.module.ts
+++ src/app/vendor/angular-material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
   imports: [],
-  exports: [MatInputModule, MatFormFieldModule],
+  exports: [MatInputModule, MatFormFieldModule, MatButtonModule],
 })
 export class AngularMaterialModule {}
