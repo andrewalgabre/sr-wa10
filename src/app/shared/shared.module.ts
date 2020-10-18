67db49fa77a292a649d89887cdbae8185ccedfc9
Andrew Algabre <a.algabre@me.com>
2020-10-18 17:17:54 +0200

    add AngularMaterial und ReactiveFormsModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NicknameEditorComponent } from './components/nickname-editor/nickname-editor.component';
+import { AngularMaterialModule } from '../vendor/angular-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [NicknameEditorComponent],
-  imports: [],
-  exports: [NicknameEditorComponent],
+  imports: [AngularMaterialModule, CommonModule, ReactiveFormsModule],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    NicknameEditorComponent,
+  ],
 })
 export class SharedModule {}
