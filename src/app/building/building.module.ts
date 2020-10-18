f24493a90a216396a1a719db3dfb7f8c696177cc
Andrew Algabre <a.algabre@me.com>
2020-10-18 17:16:56 +0200

    lint

--- src/app/building/building.module.ts
+++ src/app/building/building.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { BuildingRoutingModule } from './building-routing.module';
 import { BuildingComponent } from './containers/building/building.component';
 import { BuildingInfoComponent } from './components/building-info/building-info.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [BuildingComponent, BuildingInfoComponent],
-  imports: [
-    CommonModule,
-    BuildingRoutingModule
-  ]
+  imports: [SharedModule, BuildingRoutingModule],
 })
-export class BuildingModule { }
+export class BuildingModule {}
