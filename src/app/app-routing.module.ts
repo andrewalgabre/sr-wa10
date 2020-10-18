601f8ff2d37611895175cded6e8ae2f813ea0ed4
Andrew Algabre <a.algabre@me.com>
2020-10-18 12:41:23 +0200

    add building routing as lazyloaded module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BuildingComponent } from './building/containers/building/building.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/building', pathMatch: 'full' },
+  {
+    path: 'building',
+    loadChildren: () =>
+      import('./building/building.module').then((m) => m.BuildingModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
