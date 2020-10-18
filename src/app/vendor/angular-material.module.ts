4da53e1ee4917e7fd69e4e186586289959af57c1
Andrew Algabre <a.algabre@me.com>
2020-10-18 20:54:15 +0200

    add mat-icon module

--- src/app/vendor/angular-material.module.ts
+++ src/app/vendor/angular-material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [],
-  exports: [MatInputModule, MatFormFieldModule, MatButtonModule],
+  exports: [MatInputModule, MatFormFieldModule, MatButtonModule, MatIconModule],
 })
 export class AngularMaterialModule {}
