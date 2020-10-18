8576cc01e479938f1b2484b5b7d45245112256fd
Andrew Algabre <a.algabre@me.com>
2020-10-18 18:12:44 +0200

    add documentation

---
+++ src/app/shared/services/validator.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ValidatorService {
+  constructor() {}
+
+  /**
+   * Checks if the nickname is valid
+   * @param nickname Nickname
+   * @returns boolean observable
+   */
+  public isValidNickname(nickname: string): Observable<boolean> {
+    if (!nickname.startsWith('a')) {
+      return of(false);
+    }
+    return of(true);
+  }
+}
