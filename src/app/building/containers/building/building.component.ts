49e6105b2c329348a4a423312be5e5b9aebd6c68
Andrew Algabre <a.algabre@me.com>
2020-10-18 20:50:38 +0200

    add data in the container component

--- src/app/building/containers/building/building.component.ts
+++ src/app/building/containers/building/building.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-building',
   templateUrl: './building.component.html',
-  styleUrls: ['./building.component.scss']
+  styleUrls: ['./building.component.scss'],
 })
 export class BuildingComponent implements OnInit {
+  data = {
+    address: '30 St Mary Axe, London',
+    description:
+      'Das Viertel rund um den Mythenquai entstand vor 150 Jahren, als am nordwestlichen Ufer des Zürichsees das Sumpfgebiet trockengelegt und aufgeschüttet wurde. ',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
