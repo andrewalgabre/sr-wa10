f38e0fab72d269c649c42bb780d94289927891c7
Andrew Algabre <a.algabre@me.com>
2020-10-18 12:37:37 +0200

    add buildinginfo component

--- src/app/building/building.module.ts
+++ src/app/building/building.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { BuildingRoutingModule } from './building-routing.module';
 import { BuildingComponent } from './containers/building/building.component';
+import { BuildingInfoComponent } from './components/building-info/building-info.component';
 
 
 @NgModule({
-  declarations: [BuildingComponent],
+  declarations: [BuildingComponent, BuildingInfoComponent],
   imports: [
     CommonModule,
     BuildingRoutingModule
