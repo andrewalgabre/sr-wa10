ec815849fe6959e66577fddc50a042e9a82214ca
Andrew Algabre <a.algabre@me.com>
2020-10-18 12:44:29 +0200

    change changeDetection to onPush

--- src/app/building/components/building-info/building-info.component.ts
+++ src/app/building/components/building-info/building-info.component.ts
@@ -1,9 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-building-info',
   templateUrl: './building-info.component.html',
   styleUrls: ['./building-info.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BuildingInfoComponent implements OnInit {
   @Input() description: string = '';
